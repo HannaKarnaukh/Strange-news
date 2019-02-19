e32f1053f724455c7ea17b3d9666c3f57632af61
Hanna Karnaukh <anja.karnauh@gmail.com>
2019-02-19 09:59:47 +0200

    Added Extensions, extension: Data

---
+++ Strange news/Helpers/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extentions.swift
+//  Strange news
+//
+//  Created by Hanna Karnaukh on 2/19/19.
+//  Copyright © 2019 Hanna Karnaukh. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    mutating func prepareToDecode() {
+        guard var string = String(data: self, encoding: .utf8) else {
+            return
+        }
+        string = string.replacingOccurrences(of: "\r", with: " ").replacingOccurrences(of: "\n", with: " ")
+        self = string.data(using: .utf8)!
+    }
+}
