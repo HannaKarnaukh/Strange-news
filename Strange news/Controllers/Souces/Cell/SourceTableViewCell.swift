e43770dd5e60e70d94683cf1deb3e9ce85759fca
Hanna Karnaukh <anja.karnauh@gmail.com>
2019-02-20 07:04:59 +0200

    Added Sources Controller and Cell

---
+++ Strange news/Controllers/Souces/Cell/SourceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SourceTableViewCell.swift
+//  Strange news
+//
+//  Created by Hanna Karnaukh on 2/20/19.
+//  Copyright © 2019 Hanna Karnaukh. All rights reserved.
+//
+
+import UIKit
+
+class SourceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
