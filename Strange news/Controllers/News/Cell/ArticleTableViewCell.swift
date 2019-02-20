068fa959e4fa8e994e40f3eb039b1c9d7ea41fc8
Hanna Karnaukh <anja.karnauh@gmail.com>
2019-02-20 06:46:05 +0200

    Added ‘ArticleTableViewCell’

---
+++ Strange news/Controllers/News/Cell/ArticleTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  ArticleTableViewCell.swift
+//  Strange news
+//
+//  Created by Hanna Karnaukh on 2/20/19.
+//  Copyright © 2019 Hanna Karnaukh. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    var article: Article?
+    @IBOutlet var newsImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var authorLabel: UILabel!
+    @IBOutlet var sourceNameLabel: UILabel!
+    @IBOutlet var descriptionTextView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        newsImageView.image = nil
+        titleLabel.text = ""
+        authorLabel.text = ""
+        sourceNameLabel.text = ""
+        descriptionTextView.text = ""
+    }
+    
+    func sutup() {
+        titleLabel.text = article?.title
+        authorLabel.text = article?.author
+        sourceNameLabel.text = article?.source?.name
+        descriptionTextView.text = article?.description
+    }
+}
+
+extension ArticleTableViewCell {
+    static let identifier = String(describing: ArticleTableViewCell.self)
+    
+    static var nib: UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+}
+
