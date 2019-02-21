a1753957e95f1738bc8f8006b0afe82b4eac63c8
Hanna Karnaukh <anja.karnauh@gmail.com>
2019-02-21 12:44:25 +0200

    Init ‘FilterViewController’, ‘FilterTableViewCell’

---
+++ Strange news/Controllers/Filter/FilterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterTableViewCell.swift
+//  Strange news
+//
+//  Created by Hanna Karnaukh on 2/21/19.
+//  Copyright © 2019 Hanna Karnaukh. All rights reserved.
+//
+
+import UIKit
+
+class FilterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
