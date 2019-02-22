a0b7d04665a80115d021726d35d99835060f9204
Hanna Karnaukh <anja.karnauh@gmail.com>
2019-02-22 07:02:27 +0200

    Created all news controllers

---
+++ Strange news/Controllers/News/BaseNewsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseNewsTableViewController.swift
+//  Strange news
+//
+//  Created by Hanna Karnaukh on 2/22/19.
+//  Copyright © 2019 Hanna Karnaukh. All rights reserved.
+//
+
+import UIKit
+
+class BaseNewsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
