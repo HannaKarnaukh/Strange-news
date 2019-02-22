e8cf078155ad6a40df9fddf369dbe9e0bd0b040a
Hanna Karnaukh <anja.karnauh@gmail.com>
2019-02-22 07:26:54 +0200

    Added tableView property

--- Strange news/Controllers/News/BaseNewsTableViewController.swift
+++ Strange news/Controllers/News/BaseNewsTableViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class BaseNewsTableViewController: UIViewController {
 
+    //MARK: - Properties
+    var tableView: UITableView!
+    
+    var articles = [Article]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,5 +23,30 @@ class BaseNewsTableViewController: UIViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     
+    }
+}
+
+//MARK:- UITableViewDataSource, UITableViewDelegate
+extension BaseNewsTableViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ArticleTableViewCell.identifier, for: indexPath) as? ArticleTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.article = articles[indexPath.row]
+        cell.setup()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let url = articles[indexPath.row].url else {
+            return
+        }
+        UIApplication.shared.open(url)
     }
 }
